49ea3bd4c163b4ddb3bf069a7b64269c56dc8122
Harshavardhan Ranganathan <harshhay69pr@gmail.com>
2025-08-20 13:33:38 +0530

    Final Config

--- app.js
+++ app.js
@@ -8,8 +8,5 @@ app.use('/', schoolRoutes)
 
 
 //Server Start
-const PORT=5000
-app.listen(PORT,()=>{
-    console.log(`Server is running on the port ${PORT}`);
-    
-});
+const PORT = process.env.PORT || 5000;   // fallback to 5000 locally
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
